dc3edbb108cfbb30323067d96243c6c86e412d2f
Jonas <jasp@itu.dk>
2016-12-31 03:41:13 +0100

    Main layout

--- app/src/main/java/com/specht/jonas/ros17app/MainActivity.java
+++ app/src/main/java/com/specht/jonas/ros17app/MainActivity.java
@@ -5,18 +5,30 @@ import android.content.SharedPreferences;
 import android.support.v4.content.SharedPreferencesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPref;
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences sharedPref = getSharedPreferences(getString(R.string.user_info_file_key), MODE_PRIVATE);
+        sharedPref = getSharedPreferences(getString(R.string.user_info_file_key), MODE_PRIVATE);
         String firstName = sharedPref.getString(getString(R.string.saved_first_name), null);
         if (firstName == null) {
             startActivity(new Intent(this, UserInfoActivity.class));
         }
+
+        spinner = (Spinner) findViewById(R.id.spinner_category);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.category_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
